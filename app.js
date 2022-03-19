6521db0ef619e2fe94813d63869c8954302dc175
Vitaliy Kornienko <Kornienko.Vitaliy@gmail.com>
2022-03-19 17:01:04 +0200

    methods vs computed properties

--- app.js
+++ app.js
@@ -1,21 +1,22 @@
 new Vue({
-  el: "#app",
-  data: {
-    a: 0,
-    b: 0,
-    age: 20
-  },
-  methods: {
-
-  },
-  computed: {
-    addToA: function() {
-      console.log('addToA');
-      return this.a + this.age;
+    el: '#app',
+    data: {
+        a: 0,
+        b: 0,
+        age: 20,
     },
-    addToB: function() {
-      console.log('addToB');
-      return this.b + this.age;
-    }
-  }
-})
+    methods: {
+        //методи викликаються одночасно
+    },
+    computed: {
+        // розрахункові властивості- викликаються за потреби!
+        addToA: function () {
+            console.log('addToA');
+            return this.a + this.age;
+        },
+        addToB: function () {
+            console.log('addToB');
+            return this.b + this.age;
+        },
+    },
+});
