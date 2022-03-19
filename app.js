78d36c68b53fa98a9e3745f2e2666e1201759936
Vitaliy Kornienko <Kornienko.Vitaliy@gmail.com>
2022-03-19 13:12:23 +0200

    my own practice

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 new Vue({
-  el: "#app",
-  data: {
-    name: 'Yauhen',
-    job: 'Front-end developer'
-  },
-  methods: {
-    greet: function() {
-      return 'Hello me name is ' + this.name;
-    }
-  }
-})
+    el: '#app',
+    data: {
+        name: 'Vitaliy',
+        job: 'Front-end developer',
+    },
+    methods: {
+        greet: function () {
+            return 'Hello me name is ' + this.name;
+        },
+    },
+});
